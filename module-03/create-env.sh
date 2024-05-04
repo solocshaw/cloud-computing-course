1007bf9ce52ed017046c568116da75fadbea9097
Sol Crenshaw <solocshaw@riseup.net>
2024-05-03 21:00:35 -0500

    specify protocol for create ARN in env script

--- module-03/create-env.sh
+++ module-03/create-env.sh
@@ -36,7 +36,7 @@ echo $SUBNET2B
 
 echo 'Creating the TARGET GROUP and storing the ARN in $TARGETARN...'
 # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-target-group.html
-TARGETARN=$(aws elbv2 create-target-group --name $8)
+TARGETARN=$(aws elbv2 create-target-group --name $8 --protocol HTTP)
 
 echo "Creating ELBv2 Elastic Load Balancer..."
 #https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-load-balancer.html
