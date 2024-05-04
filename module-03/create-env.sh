918ed9e67e32aef4a52194abeefd0cd243c7c7eb
Sol Crenshaw <solocshaw@riseup.net>
2024-05-03 20:46:55 -0500

    modify create env script

--- module-03/create-env.sh
+++ module-03/create-env.sh
@@ -36,7 +36,7 @@ echo $SUBNET2B
 
 echo 'Creating the TARGET GROUP and storing the ARN in $TARGETARN...'
 # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-target-group.html
-TARGETARN=$(aws create-target-group)
+TARGETARN=$(aws elbv2 create-target-group)
 
 echo "Creating ELBv2 Elastic Load Balancer..."
 #https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-load-balancer.html
