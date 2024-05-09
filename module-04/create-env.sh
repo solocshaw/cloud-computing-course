764f893eaf688c4dca17dd957788d3f826ccf074
Sol Crenshaw <solocshaw@riseup.net>
2024-05-09 17:16:40 -0500

    debug

--- module-04/create-env.sh
+++ module-04/create-env.sh
@@ -100,12 +100,12 @@ aws autoscaling create-auto-scaling-group \
   --max-size ${15} \
   --desired-capacity ${16} \
   --vpc-zone-identifier $SUBNET2A,$SUBNET2B \
-  --target-group-arn $TARGETARN
+  --target-group-arns $TARGETARN
 
 echo 'Waiting for Auto Scaling Group to spin up EC2 instances and attach them to the TargetARN...'
 # Create waiter for registering targets
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/target-in-service.html
-aws elbv2 wait target-in-service
+aws elbv2 wait target-in-service --target-group-arn $TARGETARN
 echo "Targets attached to Auto Scaling Group..."
 
 # Collect Instance IDs
