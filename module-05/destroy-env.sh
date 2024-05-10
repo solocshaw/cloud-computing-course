2c875b7d6d18082213b2fcab28fed1b5d1768d29
Sol Crenshaw <solocshaw@riseup.net>
2024-05-09 23:32:29 -0500

    update destroy script with desired and min

--- module-05/destroy-env.sh
+++ module-05/destroy-env.sh
@@ -32,11 +32,11 @@ if [ "$ASGNAMES" != "" ]
 
       aws autoscaling update-auto-scaling-group \
         --auto-scaling-group-name $ASGNAME \
-        --min-size
+        --min-size 0
 
       aws autoscaling update-auto-scaling-group \
       --auto-scaling-group-name $ASGNAME \
-      --desired-capacity
+      --desired-capacity 0
   
      if [ "$INSTANCEIDS" != "" ]
        then
