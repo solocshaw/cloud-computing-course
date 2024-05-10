9a17ef1d2db78934f47eda64a3b4d5f49979fcb5
Sol Crenshaw <solocshaw@riseup.net>
2024-05-09 23:11:37 -0500

    debug

--- module-05/create-env.sh
+++ module-05/create-env.sh
@@ -131,11 +131,11 @@ echo "Created S3 bucket: ${20}..."
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/index.html
 # Upload illinoistech.png and rohit.jpg to bucket ${19}
 echo "Uploading image: ./images/illinoistech.png to s3://${19}..."
-aws s3 cp s3://${19}
+aws s3 cp ./images/illinoistech.png s3://${19}
 echo "Uploaded image: ./images/illinoistech.png to s3://${19}..."
 
 echo "Uploading image: ./images/rohit.jpg to s3://${19}..."
-aws s3 cp s3://${19}
+aws s3 cp ./images/rohit.jpg s3://${19}
 echo "Uploaded image: ./images/rohit.jpg to s3://${19}..."
 
 echo "Listing content of bucket: s3://${19}..."
@@ -143,11 +143,11 @@ aws s3 ls s3://${19}
 
 # Upload ranking.jpg and elevate.webp to bucket ${20}
 echo "Uploading image: ./images/elevate.webp to s3://${20}..."
-aws s3 cp s3://${20}
+aws s3 cp ./images/elevate.webp s3://${20}
 echo "Uploaded image: ./images/elevate.webp to s3://${20}..."
 
 echo "Uploading image: ./images/ranking.jpg to s3://${20}..."
-aws s3 cp s3://${20}
+aws s3 cp ./images/ranking.jpg s3://${20}
 echo "Uploaded image: ./images/ranking.jpg to s3://${20}..."
 
 echo "Listing content of bucket: s3://${20}..."
